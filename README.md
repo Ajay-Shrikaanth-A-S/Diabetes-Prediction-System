ba8c4d70a9681252ccc4d0f68106f8062c993f68
AJAY <shrikaanthajay@gmail.com>
2025-01-02 16:11:54 +0530

    added code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Diabetes Prediction System
  
+It tracks the disease based on the inputs given by the user and classifies them they are normal or diseased
---
+++ diabetes_system.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pickle 
+
+loaded_model = pickle.load(open('"C:/Users/Lenovo/Documents/Dataset/trained_model.sav"', 'rb'))
+
+
+input_data = (2,197,70,45,543,30.5,0.158,53)
+
+input_data_as_numpy_array = np.asarray(input_data)
+
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if prediction == 0:
+  print('The person is not diabetic')
+else:
+  print('The person is diabetic')
+  
